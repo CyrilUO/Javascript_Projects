304d31dde64724f753157ea2b184f125a491d594
Cyril-Dev09 <145380555+Cyril-Dev09@users.noreply.github.com>
2024-06-22 12:37:16 +0200

    Adding background loader button

--- UO-PWD-GENERATOR/password.js
+++ UO-PWD-GENERATOR/password.js
@@ -1,46 +1,60 @@
-
 function generateRandomPassword(length) {
-    let chars ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?"
+  let chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
 
-    let password = "";
+  let password = "";
 
-    for (let i = 0; i < length; i++){
-        const randomChar = Math.floor(Math.random() * chars.length);
-        password += chars[randomChar];
-        console.log(password);
-    }
+  for (let i = 0; i < length; i++) {
+    const randomChar = Math.floor(Math.random() * chars.length);
+    password += chars[randomChar];
+    console.log(password);
+  }
 
-    return password
-    
+  return password;
 }
 
-
 function displayPassword() {
+  let displayPassword = document.getElementById("pwd_input_displayer");
+  let lengthElement = document.getElementById("length");
+  let length = parseInt(lengthElement.value);
 
-    let displayPassword = document.getElementById('pwd_input_displayer');
-    let lengthElement= document.getElementById('length');
-    let length = parseInt(lengthElement.value)
+  const password = generateRandomPassword(length);
 
-    const password = generateRandomPassword(length);
-
-    if (displayPassword) {
-        displayPassword.value = password;
-    } else {
-        console.error('Element with the specified ID was not found.');
-    }
+  if (displayPassword) {
+    displayPassword.value = password;
+  } else if (error) {
+    console.error("Element with the specified ID was not found.");
+    displayPassword.value = error;
+  }
 }
 
 function updateLengthDisplay() {
-    const lengthElement = document.getElementById('length');
-    const lengthValueElement = document.getElementById('length_value');
-    lengthValueElement.textContent = lengthElement.value;
+  const lengthElement = document.getElementById("length");
+  const lengthValueElement = document.getElementById("length_value");
+  lengthValueElement.textContent = lengthElement.value;
 }
 
-
-function setPasswordConfig () {
-
-}
+function setPasswordConfig() {}
 
 function setNewBackground() {
+  let lastBackgroundIndex = null;
 
+  const imageBank = [
+    "images/mountain_cover.jpg",
+    "images/space3.jpg",
+    "images/synthwave-electro-4k-co.jpg",
+    "images/synthwave3.jpg",
+  ];
+
+  let randomIndex;
+
+  do {
+    randomIndex = Math.floor(Math.random() * imageBank.length);
+  } while (randomIndex === lastBackgroundIndex);
+
+  const newBackground = imageBank[randomIndex];
+
+  document.body.style.backgroundImage = `url(${newBackground})`;
+
+  lastBackgroundIndex = randomIndex;
 }
